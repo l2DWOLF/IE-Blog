15628e9a059583f02a25851544518f3ad6b881dd
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-06-21 11:01:04 -0400

    add - conditional rendering > redux user state test permissions

--- ie-drf-blog/src/components/home/Home.jsx
+++ ie-drf-blog/src/components/home/Home.jsx
@@ -1,12 +1,14 @@
-import Articles from "../articles/Articles"
 import "./css/home.css"
-
-
+import Articles from "../articles/Articles"
+import {useSelector} from "react-redux"
 
 function Home() {
-
+    const user = useSelector((state) => state.user.user)
     return (<div className="home-div">
         <h1>IE Blog</h1>
+
+        {user?.is_admin && <p>Hey Admin!</p>}
+        {user?.is_mod && <p>Hey Moderator!</p>}
         <Articles />
     </div>)
 }
