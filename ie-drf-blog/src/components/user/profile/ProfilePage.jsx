d779c9b0c998b8d2e9646e64218fd73993a2e72e
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-07-24 18:21:24 -0400

    added - mobile-profilepage.css,
    modified- profile page + edit modal mobile compatibility.

--- ie-drf-blog/src/components/user/profile/ProfilePage.jsx
+++ ie-drf-blog/src/components/user/profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import './css/profilepage.css';
-import '../../common/design/design-tools.css';
+import './css/mobile-profilepage.css';
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { motion, AnimatePresence } from 'motion/react';
@@ -40,7 +40,7 @@ function ProfilePage() {
                 ])
                 setUserInfo(userInfo);
                 setUserProfile(profile);
-            }
+            };
         } catch (err) {
             handleException(err);
             setPublicProfile(null);
@@ -49,7 +49,7 @@ function ProfilePage() {
             setProfileObj(null);
         } finally {
             setIsLoading(false);
-        }
+        };
     };
 
     useEffect(() => {
@@ -65,26 +65,25 @@ function ProfilePage() {
     }, [viewingOwnProfile, userInfo, userProfile, publicProfile]);
 
     const handleCancel = () => setIsEditing(false);
-
     const handleDelete = async () => {
-            const confirm = window.confirm(`Are you sure you want to delete your account?`);
-            if (!confirm) return;
-            setIsLoading(true);
-            try {
-                await deleteUser(profileObj?.id);
-                successMsg(`User ${profileObj?.username} was deleted successfully.`);
-                if(viewingOwnProfile){
-                    const autoLogout = true;
-                    logoutHandler(dispatch, autoLogout);
-                } else {
-                    fetchUser();
-                };
-            } catch (err) {
-                handleException(err);
-            } finally {
-                setIsLoading(false);
-            }
-        };
+        const confirm = window.confirm(`Are you sure you want to delete your account?`);
+        if (!confirm) return;
+        setIsLoading(true);
+        try {
+            await deleteUser(profileObj?.id);
+            successMsg(`User ${profileObj?.username} was deleted successfully.`);
+            if(viewingOwnProfile){
+                const autoLogout = true;
+                logoutHandler(dispatch, autoLogout);
+            } else {
+                fetchUser();
+            };
+        } catch (err) {
+            handleException(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
     <div className="profile-wrapper">
@@ -108,101 +107,100 @@ function ProfilePage() {
             </div>
         </div>
 
-            {isLoading ? (
-                <LoadingScreen message="Loading Profile..." />
-                ) : ( profileObj?.username ? (
-                <section className="profile-section">
-                <h2>{profileObj?.username}'s Profile</h2>
-                    <div className="profile-container">
-                        <AnimatePresence>
-                            <motion.div
-                                className="profile-card-motion"
-                                initial={{ opacity: 0, scale: 0.95, y: 20 }}
-                                animate={{ opacity: 1, scale: 1, y: 0 }}
-                                exit={{ opacity: 0, scale: 0.2, x: 450, y: 30 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <div className="profile-card">
-                                    <div className="profile-avatar-wrapper">
-                                        <img
-                                            src={profileObj?.image || "https://images.unsplash.com/photo-1717864477200-d4d1f112c792?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-                                            alt="Profile Avatar"
-                                            className="profile-avatar"
-                                        />
+        {isLoading ? (
+            <LoadingScreen message="Loading Profile..." />
+            ) : ( profileObj?.username ? (
+            <section className="profile-section">
+            <h2>{profileObj?.username}'s Profile</h2>
+                <div className="profile-container">
+                    <AnimatePresence>
+                        <motion.div
+                        className="profile-card-motion"
+                        initial={{ opacity: 0, scale: 0.95, y: 20 }}
+                        animate={{ opacity: 1, scale: 1, y: 0 }}
+                        exit={{ opacity: 0, scale: 0.2, x: 450, y: 30 }}
+                        transition={{ duration: 0.5 }}
+                        >
+                        <div className="profile-card">
+                            <div className="profile-avatar-wrapper">
+                                <img
+                                src={profileObj?.image || "https://images.unsplash.com/photo-1717864477200-d4d1f112c792?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                                alt="Profile Avatar"
+                                className="profile-avatar"
+                                />
+                            </div>
+                            {(viewingOwnProfile || user?.is_admin || user?.is_staff) && ( 
+                                <div className="profile-card-btns">
+                                    <button onClick={() => setIsEditing(true)}>
+                                        <FileEdit className="card-icons" /> Edit Profile
+                                    </button>
+                                    <button onClick={handleDelete}>
+                                        <Trash2 className="card-icons" /> Delete Account
+                                    </button>
+                                </div>
+                            )}
+
+                            {isEditing ? (
+                                <EditProfileForm profileObj={profileObj}
+                                    refetch={fetchUser} onCancel={handleCancel} />
+                            ) : (<>
+                            <div className="profile-info-container">
+                                <h3>Account Info</h3>
+                                <div className="account-info">
+                                    <div className="info-field">
+                                        <strong>User ID:</strong> <p>#{profileObj?.id}</p>
                                     </div>
-                                    {(viewingOwnProfile || user?.is_admin || user?.is_staff) && ( 
-                                        <div className="profile-card-btns">
-                                            <button onClick={() => setIsEditing(true)}>
-                                                <FileEdit className="card-icons" /> Edit Profile
-                                            </button>
-                                            <button onClick={handleDelete}>
-                                                <Trash2 className="card-icons" /> Delete Account
-                                            </button>
-                                        </div>)
+                                    <div className="info-field">
+                                        <strong>Username:</strong> <p>{profileObj?.username}</p>
+                                    </div>
+                                    {viewingOwnProfile &&
+                                        <div className="info-field">
+                                            <strong>Email:</strong> <p>{profileObj?.email}</p>
+                                        </div>
                                     }
+                                </div>
 
-                                    {isEditing ? (
-                                        <EditProfileForm profileObj={profileObj}
-                                            refetch={fetchUser} onCancel={handleCancel} />
-                                    ) : (<>
-                                        <div className="profile-info-container">
-                                            <h3>Account Info</h3>
-                                            <div className="account-info">
-                                                <div className="info-field">
-                                                    <strong>User ID:</strong> <p>#{profileObj?.id}</p>
-                                                </div>
-                                                <div className="info-field">
-                                                    <strong>Username:</strong> <p>{profileObj?.username}</p>
-                                                </div>
-                                                {viewingOwnProfile &&
-                                                    <div className="info-field">
-                                                        <strong>Email:</strong> <p>{profileObj?.email}</p>
-                                                    </div>
-                                                }
-                                            </div>
+                                <div className="badges-container">
+                                    {(profileObj?.admin_status) && (<p className="role-badge">🔧 Admin User</p>)}
+                                    {profileObj?.mod_status && (<p className="role-badge">🛡️ Moderator</p>)}
+                                    {(profileObj?.staff_admin_status) && (<p className="role-badge">👤 Admin Staff </p>)}
+                                </div>
+                            </div>
 
-                                            <div className="badges-container">
-                                                {(profileObj?.admin_status) && (<p className="role-badge">🔧 Admin User</p>)}
-                                                {profileObj?.mod_status && (<p className="role-badge">🛡️ Moderator</p>)}
-                                                {(profileObj?.staff_admin_status) && (<p className="role-badge">👤 Admin Staff </p>)}
-                                            </div>
-                                        </div>
-
-                                        <div className="profile-details-container">
-                                            <h3>Profile Details</h3>
-                                            <div className="profile-info">
-                                                <div className="info-field">
-                                                    <strong>Full Name:</strong> <p>{profileObj?.first_name} {profileObj?.last_name}</p>
-                                                </div>
-                                                <div className="info-field">
-                                                    <strong>Location:</strong> <p>{profileObj?.location}</p>
-                                                </div>
-                                                <div className="info-field">
-                                                    <strong>Birth Date:</strong> <p>{profileObj?.birth_date}</p>
-                                                </div>
-                                            </div>
-                                            <div className="info-field bio-field">
-                                                <strong>Bio:</strong> <p>{profileObj?.bio || 'N/A'}</p>
-                                            </div>
-                                        </div>
-                                    </>)}
-
-                                    <div className="profile-timestamps">
-                                        <p>Created at: {profileObj?.created_at}</p>
-                                        <p>Last update: {profileObj?.updated_at}</p>
+                            <div className="profile-details-container">
+                                <h3>Profile Details</h3>
+                                <div className="profile-info">
+                                    <div className="info-field">
+                                        <strong>Full Name:</strong> <p>{profileObj?.first_name} {profileObj?.last_name}</p>
+                                    </div>
+                                    <div className="info-field">
+                                        <strong>Location:</strong> <p>{profileObj?.location}</p>
+                                    </div>
+                                    <div className="info-field">
+                                        <strong>Birth Date:</strong> <p>{profileObj?.birth_date}</p>
                                     </div>
                                 </div>
-                            </motion.div>
-                        </AnimatePresence>
-                    </div> 
-            </section>) : (
-                <div className="loading-wrapper">
-                    <p>User doesn't exist...</p>
-                    <button onClick={() => navigate("/")}>
-                        <Home className="card-icons" /> Home
-                    </button>
-                </div>)
-            )}
+                                <div className="info-field bio-field">
+                                    <strong>Bio:</strong> <p>{profileObj?.bio || 'N/A'}</p>
+                                </div>
+                            </div> </>)}
+
+                            <div className="profile-timestamps">
+                                <p>Created at: {profileObj?.created_at}</p>
+                                <p>Last update: {profileObj?.updated_at}</p>
+                            </div>
+                        </div>
+                        </motion.div>
+                    </AnimatePresence>
+                </div> 
+        </section>) : (
+            <div className="loading-wrapper">
+                <p>User doesn't exist...</p>
+                <button onClick={() => navigate("/")}>
+                    <Home className="card-icons" /> Home
+                </button>
+            </div>)
+        )}
     </div>
     );
 }
